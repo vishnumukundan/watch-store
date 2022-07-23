94810f927f3c49ca080ab1580734d719963b13b1
Vishnu Mukundan <vishnumukundan040@gmail.com>
2022-07-24 01:23:55 +0530

    added screenshots and modified readme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:watch_store/pages/home/home.dart';
+import 'package:watch_store/theme/config/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +11,12 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: kColorScaffold,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: kColorScaffold,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Raleway'),
